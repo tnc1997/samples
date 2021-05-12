e31ae4c885171b0e651c7a15fbb9257d4bbaee8c
Thomas Clark <tnc1997@virginmedia.com>
2021-05-12 20:15:59 +0100

    feat: add creating an identityserver project

---
+++ creating-an-identityserver-project/IdentityServer/Startup.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using Duende.IdentityServer.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddInMemoryClients(new List<Client>())
+                .AddInMemoryApiScopes(new List<ApiScope>());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+        }
+    }
+}
---
+++ creating-an-identityserver-project/README.md
@@ -0,0 +1,7 @@
+# Creating an IdentityServer Project
+
+## Getting Started
+
+1. `dotnet dev-certs https --export-path $env:USERPROFILE\.aspnet\https\aspnetapp.pfx --password password`
+1. `dotnet dev-certs https --trust`
+1. `docker compose up --build`
