ff3d7072de1386ce6fb43cc31abb868067f9da20
Thomas Clark <tnc1997@virginmedia.com>
2022-04-22 21:49:29 +0100

    fix: diagnostics page not working outside of localhost

--- creating-an-identityserver-6-solution/IdentityServer/Pages/Diagnostics/Index.cshtml.cs
+++ creating-an-identityserver-6-solution/IdentityServer/Pages/Diagnostics/Index.cshtml.cs
@@ -13,12 +13,6 @@ public class Index : PageModel
 
     public async Task<IActionResult> OnGet()
     {
-        var localAddresses = new[] { "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
-        if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
-        {
-            return NotFound();
-        }
-
         View = new ViewModel(await HttpContext.AuthenticateAsync());
 
         return Page();
